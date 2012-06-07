80e62310bd4f953f37fd436158007ee253d48771
ArnoldOverwater <arnoldoverwater@gmail.com>
2012-06-07 15:54:05 +0200

    Pearson is now a class

--- src/Pearson.java
+++ src/Pearson.java
@@ -1,95 +1,84 @@
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.TreeMap;
 
 
 public class Pearson {
 	
-	public static List<Map.Entry<Integer, Double>> getRecommendations(UserPreferences user, Map<Integer, UserPreferences> mapUP) {
-		Map<Integer, Double> pearsonsRatings = getPearsonsRatings(user, mapUP);
-		List<Map.Entry<Integer, Double>> entryList = new ArrayList<Entry<Integer, Double>>(pearsonsRatings.entrySet());
-		Collections.sort(entryList, new Comparator<Map.Entry<Integer, Double>>() {
-			@Override
-			public int compare(Entry<Integer, Double> o1, Entry<Integer, Double> o2) {
-				if (o1.getValue() > o2.getValue())
-					return -1;
-				else if (o1.getValue() < o2.getValue())
-					return 1;
-				else
-					return 0;
+	public int userId1;
+	
+	public int userId2;
+	
+	public int[] itemIds;
+	
+	public double[] ratings1;
+	
+	public double[] ratings2;
+	
+	public double pearson;
+	
+	public Pearson(UserPreferences user1, UserPreferences user2) {
+		intersection(user1, user2);
+		if (itemIds.length == user1.getItemIds().length && itemIds.length == user2.getItemIds().length) {
+			return;
+		}
+		pearson = calculatePearson();
+	}
+
+	private void intersection(UserPreferences user1, UserPreferences user2) {
+		int[] originalItemIds1 = user1.getItemIds(), originalItemIds2 = user2.getItemIds();
+		double[] originalRatings1 = user1.getRatings(), originalRatings2 = user2.getRatings();
+		int[] intersectItemIds;
+		double[] intersectRatings1, intersectRatings2;
+		if (originalItemIds1.length < originalItemIds2.length) {
+			intersectItemIds = new int[originalItemIds1.length];
+			intersectRatings1 = new double[originalItemIds1.length];
+			intersectRatings2 = new double[originalItemIds1.length];
+		} else {
+			intersectItemIds = new int[originalItemIds2.length];
+			intersectRatings1 = new double[originalItemIds2.length];
+			intersectRatings2 = new double[originalItemIds2.length];
+		}
+		int index1 = 0, index2 = 0, indexResult = 0;
+		while (index1 < originalItemIds1.length && index2 < originalItemIds2.length) {
+			if (originalItemIds1[index1] < originalItemIds2[index2]) {
+				index1++;
+			} else if (originalItemIds1[index1] > originalItemIds2[index2]) {
+				index2++;
+			} else {
+				intersectItemIds[indexResult] = originalItemIds1[index1];
+				intersectRatings1[indexResult] = originalRatings1[index1++];
+				intersectRatings2[indexResult] = originalRatings2[index2++];
+				indexResult++;
 			}
-		});
-		return entryList;
+		}
+		itemIds = new int[indexResult];
+		ratings1 = new double[indexResult];
+		ratings2 = new double[indexResult];
+		System.arraycopy(intersectItemIds, 0, itemIds, 0, indexResult);
+		System.arraycopy(intersectRatings1, 0, ratings1, 0, indexResult);
+		System.arraycopy(intersectRatings2, 0, ratings2, 0, indexResult);
 	}
 	
-	public static Map<Integer, Double> getPearsonsRatings(UserPreferences user, Map<Integer, UserPreferences> mapUP) {
-		Map<Integer, Double> pearsons = getAllPearson(user, mapUP.values());
-		int[] itemIds = user.getItemIds();
-		Map<Integer, Double> pearsonsRatings = new TreeMap<Integer, Double>();
-		for (int userId : mapUP.keySet()) {
-			UserPreferences otherUser = mapUP.get(userId);
-			int[] items = exclusion(itemIds, otherUser.getItemIds());
-			for (int item : items) {
-				double pearsonRating = otherUser.getRatingForItem(item) * pearsons.get(userId);
-				pearsonsRatings.put(item, pearsonRating + (pearsonsRatings.containsKey(item) ? pearsonsRatings.get(item) : 0));
-			}
-		}
-		return pearsonsRatings;
-	}
-	
-	public static Map<Integer, Double> getAllPearson(UserPreferences user, Collection<UserPreferences> userCollection) {
-		Map<Integer, Double> pearsons = new TreeMap<Integer, Double>();
-		for (UserPreferences up : userCollection) {
-			pearsons.put(up.getUserId(), pearson(user, up));
-		}
-		return pearsons;
-	}
-	
-	public static double pearson(UserPreferences user1, UserPreferences user2) {
-		int[] items = intersection(user1.getItemIds(), user2.getItemIds());
-		if (items.length == user1.getItemIds().length && items.length == user2.getItemIds().length) {
-			return 0;
-		}
-		double[] ratings1 = new double[items.length], ratings2 = new double[items.length];
+	private double calculatePearson() {
 		double total1 = 0, total2 = 0;
-		for (int i = 0; i < items.length; i++) {
-			ratings1[i] = user1.getRatingForItem(items[i]);
-			ratings2[i] = user2.getRatingForItem(items[i]);
+		for (int i = 0; i < itemIds.length; i++) {
 			total1 += ratings1[i];
 			total2 += ratings2[i];
 		}
-		double average1 = total1 / items.length, average2 = total2 / items.length;
+		double average1 = total1 / itemIds.length, average2 = total2 / itemIds.length;
 		double numerator = 0, denominator1 = 0, denominator2 = 0;
-		for (int i = 0; i < items.length; i++) {
+		for (int i = 0; i < itemIds.length; i++) {
 			double calc1 = ratings1[i] - average1, calc2 = ratings2[i] - average2;
 			numerator += calc1 * calc2;
 			denominator1 += calc1 * calc1;
 			denominator2 += calc2 * calc2;
 		}
-		return numerator / Math.sqrt(denominator1 * denominator2);
-	}
-
-	private static int[] intersection(int[] itemIds1, int[] itemIds2) {
-		int[] intersection = new int[itemIds1.length < itemIds2.length ? itemIds1.length : itemIds2.length];
-		int index1 = 0, index2 = 0, indexResult = 0;
-		while (index1 < itemIds1.length && index2 < itemIds2.length) {
-			if (itemIds1[index1] < itemIds2[index2]) {
-				index1++;
-			} else if (itemIds1[index1] > itemIds2[index2]) {
-				index2++;
-			} else {
-				intersection[indexResult++] = itemIds1[index1++];
-				index2++;
-			}
-		}
-		int[] result = new int[indexResult];
-		System.arraycopy(intersection, 0, result, 0, indexResult);
-		return result;
+		double denominator = Math.sqrt(denominator1 * denominator2);
+		if (denominator == 0.0)
+			return 0;
+		else
+			return numerator / denominator;
 	}
 	
 	private static int[] exclusion(int[] itemIds1, int[] itemIds2) {
@@ -111,6 +100,14 @@ public class Pearson {
 		int[] result = new int[indexResult];
 		System.arraycopy(exclusion, 0, result, 0, indexResult);
 		return result;
+	}
+	
+	public static Map<Integer, Pearson> getAllPearson(UserPreferences user, Collection<UserPreferences> userCollection) {
+		Map<Integer, Pearson> pearsons = new TreeMap<Integer, Pearson>();
+		for (UserPreferences up : userCollection) {
+			pearsons.put(up.getUserId(), new Pearson(user, up));
+		}
+		return pearsons;
 	}
 
 }
